2ec5e2e095a8c2cb2f947387817f44bbf4af4593
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-07-28 18:18:26 +0900

    userモデルのバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX  = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/
+  VALID_NAME_REGEX      = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA_NAME_REGEX = /\A[ァ-ヶー]+\z/
+
+  validates :nickname,           presence: true
+  validates :encrypted_password, presence: true, format: { with: VALID_PASSWORD_REGEX  }
+  validates :last_name,          presence: true, format: { with: VALID_NAME_REGEX      }
+  validates :first_name,         presence: true, format: { with: VALID_NAME_REGEX      }
+  validates :kana_last_name,     presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  validates :kana_first_name,    presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  validates :date_of_birth,      presence: true
+  
 end
