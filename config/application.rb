97c7a8936ee5066b47d98446ee9db4578369884c
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-07-31 19:30:42 +0900

    不要なコードを削除
    require 'gimei'とFaker::Config.locale = 'ja'は必要なくなったので削除する。

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Furima39610
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    # Faker::Config.locale = 'ja'
-    # require 'gimei'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
