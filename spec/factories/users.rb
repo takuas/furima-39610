eccc9333126c31b8a08c2e1e39873114fcfcff18
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-07-31 19:37:34 +0900

    単体テストコードのダミーデータの設定変更
    last_nameとfirst_nameを生成するデータをfakerからgimeiに修正した。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { "#{Faker::Internet.password(min_length: 6, mix_case: false)}1a" }
     password_confirmation { password }
-    last_name             { Faker::Name.last_name }
-    first_name            { Faker::Name.first_name }
+    last_name             { Gimei.last.kanji }
+    first_name            { Gimei.first.kanji }
     kana_last_name        { Gimei.last.katakana }
     kana_first_name       { Gimei.first.katakana }
     date_of_birth         { Faker::Date.birthday(min_age: 18, max_age: 65) }
