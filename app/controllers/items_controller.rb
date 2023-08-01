eaf059857a8086b0d157b472faca40d5f6db82dd
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-01 16:58:56 +0900

    商品出品機能のアクション設定
    値を作成するnewアクション、データを保存するcreateアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,32 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :item_name,
+      :description,
+      :category_id,
+      :status_id,
+      :delivery_charge_id,
+      :ship_from_id,
+      :days_to_ship_id,
+      :price
+    ).merge(
+      user_id: current_user.id
+    )
   end
 end
