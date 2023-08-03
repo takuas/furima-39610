c8424e09c71d047c062ad8d17bbd605522292443
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-03 16:40:25 +0900

    商品一覧を表示するヘルバーメソッドの作成
    item_listsメソッドを作成

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def item_lists(items)
+    html = ''
+    items.each do |item|
+      html += render(partial: 'item', locals: { item: item })
+    end
+    return raw(html)
+  end
 end
