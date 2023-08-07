cfa59367a844b55809f2f5bb8c511451ffdd6c70
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-07 21:29:54 +0900

    ファクトリーボットを作成
    order_sending_addressモデルの単体テストに使うダミーデータを作成

---
+++ spec/factories/order_sending_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_sending_address do
+    postal         { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    ship_from_id   { ShipFrom.where.not(id: 1).sample.id }
+    municipalities { Faker::Address.street_address }
+    address        { Faker::Address.secondary_address }
+    tel            { Faker::Number.unique.number(digits: 11) }
+    building       { Faker::Address.building_number }
+  end
+end
