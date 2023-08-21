bbf740ae28cfb7c46b384efe7742dc070ef5c68a
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-21 11:48:06 +0900

    ヘルパーメソッドの設定修正

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def item_lists(items)
     html = ''
     items.each do |item|
-      html += render(partial: 'item', locals: { item: })
+      html += render(partial: 'item', locals: { item: item })
     end
     raw(html)
   end
