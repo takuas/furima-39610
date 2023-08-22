7b3c4929e5b822c42f1b213392d0bddb9148aab5
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-22 18:03:25 +0900

    アンダーラインを追加するjavascript

---
+++ app/javascript/header_underline.js
@@ -0,0 +1,72 @@
+function underline(){
+  const underLineList = [
+    document.querySelector('.category-list'),
+    document.getElementById("brand-list"),
+  ]
+
+  const logouts = [
+    document.getElementById("user-nickname"),
+    document.getElementById("logout")
+  ]
+
+  const logins = [
+    document.getElementById("login"),
+    document.getElementById("sign-up")
+  ]
+
+  const itemUnder = document.getElementById("item-under")
+
+  underLineList.forEach((under) => {
+    if (under) {
+      under.addEventListener('mouseover', () => {
+        under.style.textDecoration = "underline"
+      })
+
+      under.addEventListener('mouseout', () => {
+        under.style.textDecoration = ""
+      })
+    } else {
+      return
+    }
+  })
+
+  if (itemUnder) {
+    itemUnder.addEventListener('mouseover', () => {
+      itemUnder.style.textDecoration = "underline";
+    })
+  
+    itemUnder.addEventListener('mouseout', () => {
+      itemUnder.style.textDecoration = "";
+    })
+  }
+
+  logouts.forEach((logout) => {
+    if (logout) {
+      logout.addEventListener('mouseover', () => {
+        logout.style.textDecoration = "underline"
+      })
+  
+      logout.addEventListener('mouseout', () => {
+        logout.style.textDecoration = ""
+      })
+    } else {
+      return
+    }
+  })  
+
+  logins.forEach((login) => {
+    if (login) {
+      login.addEventListener('mouseover', () => {
+      login.style.textDecoration = "underline"
+      })
+  
+      login.addEventListener('mouseout', () => {
+      login.style.textDecoration = ""
+      })
+    } else {
+      return
+    }
+  })  
+
+}
+window.addEventListener('turbo:load', underline )
