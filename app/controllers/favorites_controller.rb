9f2a1505a290f9f09907f38a29c392d607d1c70c
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-29 18:11:31 +0900

    favoritesコントローラー設定
    お気に入り機能

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,21 @@
+class FavoritesController < ApplicationController
+  before_action :set_item
+
+  def create
+    favorite = current_user.favorites.build(item_id: params[:item_id])
+    favorite.save
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to item_path(@item)
+  end
+
+  private
+
+  def set_item
+    @item = Item.includes(:user).find(params[:item_id])
+  end
+end
