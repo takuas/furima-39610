e9283eefa783d7c6e3150c4c162cdb86cacb2a93
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-08-31 15:42:36 +0900

    マイページで色々表示するためのshowアクション設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_q, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+    @items = @user.items
+    @order_items = @user.order_items
+  end
+
+  private
+
+  def set_q
+    if params[:q]&.dig(:item_name)
+      squished_keywords = params[:q][:item_name].squish
+      params[:q][:item_name_cont_any] = squished_keywords.split(' ')
+    end
+    @q = Item.ransack(params[:q])
+  end
 end
